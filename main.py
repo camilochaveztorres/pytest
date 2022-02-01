b9a69945564a8f5cdf8b45920fc9c8d24c034288
camilochaveztorres <93767257+camilochaveztorres@users.noreply.github.com>
2022-02-01 09:26:35 -0300

    Fixed some stuff

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ banken = bank.Bank()
 
 
 def get_menu():
-
     run = True
     print("Welcome to the bank!")
     while run:
@@ -30,102 +29,67 @@ def get_menu():
               "7.Get account\n"
               "8.Withdraw /Deposit money\n9.Close account\n0.Exit")
         try:
-            add_new_customer("Camilo Chavez", 9304300505)
-            add_new_customer("Anna Marnfeldt", 9805453224)
-            add_new_customer("Cristina Chavez", 98010121054)
-
+            banken.add_customer('Camilo Chavez', 9304300505)
+            banken.add_customer('Anna Marnfeldt', 9805453224)
+            banken.add_customer('Cristina Chavez', 98010121054)
             answer = int(input("Type in your choice:\n"))
             if answer == 1:
-                get_all_customers()
+                banken.get_customers()
 
             elif answer == 2:
                 pnr = input("Insert your pnr:\n")
-                get_specific_customer(pnr)
+                banken.show_customer_info(pnr)
 
             elif answer == 3:
                 name = input("Insert your name:\n")
                 pnr = input("Insert your pnr:\n")
-                add_new_customer(name, pnr)
+                banken.add_customer(name, pnr)
 
             elif answer == 4:
-                name = input("Insert your new name:\n")
+                new_name = input("Insert your new name:\n")
                 pnr = input("Insert your pnr:\n")
-                change_customer_name(name, pnr)
+                if banken.change_customer_name(new_name, pnr) == True:
+                    print(f"Your new name is {new_name}")
 
             elif answer == 5:
                 pnr = input("Insert your pnr:\n")
-                delete_customer(pnr)
+                banken.remove_customer(pnr)
 
             elif answer == 6:
                 pnr = input("Insert your pnr:\n")
-                add_account_to_customer(pnr)
+                banken.add_account(pnr)
 
             elif answer == 7:
                 pnr = input("Insert your pnr\n")
                 account_nr = input("Insert you account number\n")
-                get_account(pnr, account_nr)
+                banken.get_account(pnr, account_nr)
 
             elif answer == 8:
-                pnr = input("Insert your pnr:\n")
-                account_nr = input("Insert your account number:\n")
-                amount = int(input("Insert the amount of money you want to deposit:\n"))
-                deposit_money(pnr, account_nr, amount)
+                val = input("Type 1 to withdraw - Type 2 to deposit\n")
+                if val == 1:
+                    pnr = input("Insert your pnr:\n")
+                    account_nr = input("Insert your account number:\n")
+                    amount = int(input("Insert the amount of money you want to withdraw:\n"))
+                    banken.withdrawal(pnr, account_nr, amount)
+                elif val == 2:
+                    pnr = input("Insert your pnr:\n")
+                    account_nr = input("Insert your account number:\n")
+                    amount = int(input("Insert the amount of money you want to deposit:\n"))
+                    banken.deposit(pnr, account_nr, amount)
+                else:
+                    print("Error")
 
-                pnr = input("Insert your pnr:\n")
-                account_nr = input("Insert your account number:\n")
-                amount = int(input("Insert the amount of money you want to withdraw:\n"))
-                withdraw_money(pnr, account_nr, amount)
 
             elif answer == 9:
                 pnr = input("Insert your pnr:\n")
                 account_nr = input("Insert your account number:\n")
-                close_account(pnr, account_nr)
+                banken.close_account(pnr, account_nr)
 
             else:
                 print("WHAT ARE YOU DOING?")
 
         except ValueError:
             print("Please be kind to the app and only use numbers")
-
-
-def get_all_customers():
-    banken.get_customers()
-
-
-def get_specific_customer(pnr):
-    banken.show_customer_info(pnr)
-
-
-def add_new_customer(name, pnr):
-    banken.add_customer(name, pnr)
-
-
-def change_customer_name(new_name, pnr):
-    banken.change_customer_name(new_name, pnr)
-
-
-def delete_customer(pnr):
-    banken.remove_customer(pnr)
-
-
-def add_account_to_customer(pnr):
-    banken.add_account(pnr)
-
-
-def deposit_money(pnr, account_nr, amount):
-    banken.deposit(pnr, account_nr, amount)
-
-
-def withdraw_money(pnr, account_nr, amount):
-    banken.withdrawal(pnr, account_nr, amount)
-
-
-def close_account(pnr, account_nr, ):
-    banken.close_account(pnr, account_nr)
-
-
-def get_account(pnr, account_nr):
-    banken.get_account(pnr, account_nr)
 
 
 get_menu()
